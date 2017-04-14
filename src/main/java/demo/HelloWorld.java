a319aa915fd3b3b27bbc24899a99e9f42307b27d
MrPengWei <1878729399@qq.com>
2017-04-14 15:01:06 +0800

    Update HelloWorld.java

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("hello,world pdd 555!");
+		System.out.println("hello,world pdd 222!");
 	}
 }
