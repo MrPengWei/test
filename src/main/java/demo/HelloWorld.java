b662e5a2b50fb6c36f60a4709bb1caeba8f24662
MrPengWei <1878729399@qq.com>
2017-04-14 15:46:26 +0800

    test

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -2,9 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("hello,world pdd 666666!");
-		System.out.println("hello,world pdd 555555555!");
-		System.out.println("what can i do for you");
-		System.out.println("you can do nothing for me!");
+		System.out.println("1");
 	}
 }
