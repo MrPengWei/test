d48bd1e5d1f05c066b35ee32d16d2938f4822cb9
MrPengWei <1878729399@qq.com>
2017-04-14 14:27:14 +0800

    Update HelloMyself.java

--- src/main/java/demo/HelloMyself.java
+++ src/main/java/demo/HelloMyself.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloMyself {
 	public static void main(String[] args){
-		System.out.println("hello,myself 11 cc");
+		System.out.println("hello,myself 11 c gggg");
 	}
 }
