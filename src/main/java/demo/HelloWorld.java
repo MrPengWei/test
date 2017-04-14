a5ce3374ba8b108ca7c1b5873252ddcf4d894de1
MrPengWei <1878729399@qq.com>
2017-04-14 15:36:50 +0800

    test

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -2,7 +2,7 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("hello,world pdd 555555555!");
+		System.out.println("hello,world pdd 666666!");
 		System.out.println("hello,world pdd 555555555!");
 	}
 }
